d3aaebe02c73f3c68ea39e14e076ee1c77dcaf1d
Dennis Knutsson <dennisknutsson@users.noreply.github.com>
2018-02-22 17:01:57 +0100

    Update Take_snapshot_of_VM.ps1

--- Take_snapshot_of_VM/Take_snapshot_of_VM.ps1
+++ Take_snapshot_of_VM/Take_snapshot_of_VM.ps1
@@ -18,7 +18,7 @@ if($global:DefaultVIServers.Count -lt 1)
     # vCenter Credentials
     $cred= get-credential -Message "Enter your login to vCenter:"
 
-    $vCenter = "VC"
+    $vCenter = "<change_me>"
     [void](Connect-VIServer $vCenter -Credential $cred )
     #Connect-VIServer $vCenter | Out-Null
     Clear-Host
